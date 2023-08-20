21705a6b7c1143edbea2960ff25495a8ce989a5e
Lrookie413 <106050038+Lrookie413@users.noreply.github.com>
2023-08-20 23:55:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJHgl6Vwipx-L1oeV3IFcaFibkbc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Xz5JboX_T_bEOxKBrH9QUNroEcK861uezKHD57wwKGA',
+      useTemplateId: 'Fxqv79BinzeIPiCwoCnaFGHOyHIdQ_jACUsy6BevJqI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-02',
       festivals: [
